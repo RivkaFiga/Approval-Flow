c03e0d47a3817aa19e2088103d07b91492839684
RivkaFiga <rvk2994@gmail.com>
2026-07-08 21:39:40 +0300

    Restore gateway controllers and endpoints

---
+++ src/gateway/ApprovalFlow.Gateway/Controllers/IntakeController.cs
@@ -0,0 +1,24 @@
+using ApprovalFlow.Contracts.Invocation.V1;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApprovalFlow.Gateway.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class IntakeController : ControllerBase
+{
+    private readonly DaprClient _dapr;
+
+    public IntakeController(DaprClient dapr) => _dapr = dapr;
+
+    [HttpPost]
+    [ProducesResponseType(typeof(SubmitInvoiceResponse), StatusCodes.Status202Accepted)]
+    public async Task<IActionResult> Submit([FromBody] SubmitInvoiceRequest request, CancellationToken ct)
+    {
+        var response = await _dapr.InvokeMethodAsync<SubmitInvoiceRequest, SubmitInvoiceResponse>(
+            HttpMethod.Post, "intake", "api/Intake", request, ct);
+
+        return Accepted(response);
+    }
+}
--- src/gateway/ApprovalFlow.Gateway/Program.cs
+++ src/gateway/ApprovalFlow.Gateway/Program.cs
@@ -4,8 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults("ApprovalFlow.Gateway");
 
+builder.Services.AddControllers();
+builder.Services.AddDaprClient();
+
 var app = builder.Build();
 
 app.UseServiceDefaults();
+app.MapControllers();
 
 app.Run();
