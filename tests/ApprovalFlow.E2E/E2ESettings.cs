31000d4e96aa444d83c770beb47f8ed74baa6729
RivkaFiga <149902554+RivkaFiga@users.noreply.github.com>
2026-07-10 12:01:49 +0300

    Remove unused properties from E2ESettings

--- tests/ApprovalFlow.E2E/E2ESettings.cs
+++ tests/ApprovalFlow.E2E/E2ESettings.cs
@@ -2,12 +2,6 @@ namespace ApprovalFlow.E2E;
 
 internal sealed class E2ESettings
 {
-    public string GatewayBaseUrl      { get; init; } = "http://localhost:5100";
-    public string NotificationBaseUrl { get; init; } = "http://localhost:5106";
-    public string ApprovalBaseUrl     { get; init; } = "http://localhost:5102";
-    public int    HealthTimeoutSeconds { get; init; } = 30;
-    public int    FlowTimeoutSeconds   { get; init; } = 60;
-    public int    PollIntervalMs       { get; init; } = 500;
     public string GatewayBaseUrl       { get; init; } = "http://localhost:5100";
     public string NotificationBaseUrl  { get; init; } = "http://localhost:5106";
     public int    HealthTimeoutSeconds  { get; init; } = 30;
